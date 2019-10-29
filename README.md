ef52f9057134cea2cc59017ced5630a6225a075b
Pierlauro Sciarelli <pierlauro.sciarelli@bsc.es>
2019-10-29 11:05:42 +0100

    Simplified Python grpc messages generation

---
+++ README.md
@@ -0,0 +1,11 @@
+## dataClay grpc
+
+Common grpc protocol buffers for dataclay
+
+### Usage
+
+Generate grpc messages for [pyclay](https://github.com/bsc-dom/pyclay).
+
+```sh
+bash grpc_compile_python.sh <pyclay_src_path>
+```
--- grpc_compile_python.sh
+++ grpc_compile_python.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
-function generateMessagesAndGRPC {
-	SERVERNAME=$1
-	MESSAGES_NAME="_messages"
-	echo "Cleaning $SERVERNAME messages..."
-	rm -rf ./pyclay/dataclay/communication/grpc/messages/$SERVERNAME/$SERVERNAME_pb2_grpc.py
-	rm -rf ./pyclay/dataclay/communication/grpc/messages/$SERVERNAME/$SERVERNAME_pb2.py
-	echo "Compiling $SERVERNAME messages..."
-	# Probably for the messages we can remove --grpc_python_out
-	python -m grpc.tools.protoc -I=./src/ --proto_path=.  --python_out=./pyclay/src/ --grpc_python_out=./pyclay/src/ ./src/dataclay/communication/grpc/messages/$SERVERNAME/$SERVERNAME$MESSAGES_NAME.proto
-	echo "Cleaning $SERVERNAME grpc..."
-	rm -rf ./pyclay/dataclay/communication/grpc/generated/$SERVERNAME/$SERVERNAME_messages_pb2_grpc.py
-	rm -rf ./pyclay/dataclay/communication/grpc/generated/$SERVERNAME/$SERVERNAME_messages_pb2.py
-	echo "Compiling $SERVERNAME grpc..."
-	python -m grpc.tools.protoc -I=./src/ --proto_path=. --python_out=./pyclay/src/ --grpc_python_out=./pyclay/src/ ./src/dataclay/communication/grpc/generated/$SERVERNAME/$SERVERNAME.proto
-}
+if [[ -z $1 ]]; then
+	echo "Usage: bash $0 <pyclay_src_path>"
+	exit
+fi
 
-echo "Cleaning common messages..."
-rm -rf ./pyclay/dataclay/communication/grpc/messages/common/common_messages_pb2_grpc.py
-rm -rf ./pyclay/dataclay/communication/grpc/messages/common/common_messages_pb2.py
-echo "Compiling common messages..."
-python -m grpc.tools.protoc -I=./src/ --proto_path=. --python_out=./pyclay/src/ --grpc_python_out=./pyclay/src/ ./src/dataclay/communication/grpc/messages/common/common_messages.proto
+PYCLAY_SRC=$1
+PROTOS_ROOT="./protos"
 
-
-generateMessagesAndGRPC "dataservice"
-generateMessagesAndGRPC "logicmodule"
-
+for PROTO_FILE in $(find $PROTOS_ROOT -name '*proto')
+do
+	echo "Compiling $PROTO_FILE"
+	python -m grpc.tools.protoc -I=$PROTOS_ROOT --python_out=$PYCLAY_SRC --grpc_python_out=$PYCLAY_SRC $PROTO_FILE
+done
